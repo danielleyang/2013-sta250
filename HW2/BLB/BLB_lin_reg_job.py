8818c04d342a6520585d565228cefc3b8836a967
Nick Ulle <nick.ulle@gmail.com>
2013-11-09 22:32:31 -0800

    Generalize HW2 script to any data set.

--- HW2/BLB/BLB_lin_reg_job.py
+++ HW2/BLB/BLB_lin_reg_job.py
@@ -5,11 +5,11 @@
 import argparse as ap
 import csv
 
-# External modules
+# ----- External modules
 import numpy as np
-from wls import *
+from wls import wls
 
-# Global variables
+# ----- Global constants
 # Number of little bootstraps.
 S = 5
 # Samples per little bootstrap.
@@ -17,77 +17,64 @@ R = 50
 # Exponent determining number of unique elements per little bootstrap.
 GAMMA = 0.7
 # The mini data file.
-MINI_FILE = 'data/blb_lin_reg_mini.txt'
-MINI_NCOL = 41
+MINI_N = 10 ** 4
+MINI_P = 40
+MINI_IN = 'data/blb_lin_reg_mini.txt'
+MINI_OUT = 'output/coef_mini.txt'
 # The full data file.
-FILE = ''
-NCOL = 101
+FULL_N = 10 ** 6
+FULL_P = 100
+FULL_IN = '/home/pdbaines/data/blb_lin_reg_data.txt'
 
+# ----- Functions
 def main():
-    # Set up command line arguments.
-    parser = ap.ArgumentParser(
-        description = 'This program takes a little bootstrap sample from the \
-        specified CSV data set and fits a linear regression. The last column \
-        of the data set is assumed to be the response. When no arguments are \
-        specified, the mini data set is used, provided it is in the data \
-        sub-directory.')
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('-i', metavar = 'JOB',
-                       help = 'job number (using the full data set)')
-    group.add_argument('-f', nargs = 2, metavar = ('FILE', 'NCOL'),
-                       help = 'use NCOL columns from FILE as the data set')
-    args = parser.parse_args()
+    # Process command line arguments.
+    (n, p, in_file, out_file, seeds) = parseArgs()
 
-    if args.i:
-        file = FILE
-        ncol = NCOL
-        print 'This is job {0}; using full data set...'.format(args.i)
-    elif args.f:
-        file = args.f[0]
-        ncol = int(args.f[1])
-        print 'Using {0} columns from file:'.format(ncol)
-        print 2 * ' ' + file
-    else:
-        file = MINI_FILE
-        ncol = MINI_NCOL
-        print 'No job specified; using mini data set...'
-
-    n = countLines(file)
-
-    # Draw a little bootstrap seed of size b = n^GAMMA.
+    # Draw a little bootstrap subset of size b = n^GAMMA.
     b = int(np.ceil(n ** GAMMA))
-    print 'n = {0};  b = {1}'.format(n, b)
+    print 'n = {0}, p = {1}, b = {2}'.format(n, p, b)
 
+    if seeds:
+        # Ensure every job with the same s_index draws the same subset.
+        np.random.seed(seeds[0])
     rows = np.random.choice(n, b)
 
-    # Get the seed rows.
-    data = readRows(file, rows, ncol)
+    # Get the subset rows.
+    data = readRows(in_file, rows, p + 1)
     y = data[:, -1]
     X = data[:, :-1]
     
-    print 'y shape is {0};  X shape is {1}'.format(y.shape, X.shape)
+    print 'y shape is {0};  X shape is {1}\n'.format(y.shape, X.shape)
 
-    # Get the weights for each seed row.
+    # Draw weights for each subset row.
+    if seeds:
+        # Ensure every job draws different weights.
+        np.random.seed(seeds[1])
     weights = np.random.multinomial(n, np.ones(b) / b)
 
     # Perform the linear regression.
     # wls(y, X, w, verbose=False):
     beta = wls(y, X, weights, False)
 
-    s_index = 5; r_index = 1
-    outfile = 'output/coef_{0:02d}_{1:02d}.txt'.format(s_index, r_index)
+    # Store the result.
+    with open(out_file, 'w') as writer:
+        for elt in beta:
+            writer.write(str(elt) + '\n')
+
+    print 'Completed successfully.'
 
 def readRows(file, rows, ncol):
     # Read the specified rows from a file.
     max_row = np.max(rows)
     data = np.zeros((len(rows), ncol))
     i = 0
-    with open(file, 'r') as lines:
-        lines = csv.reader(lines)
+    with open(file, 'r') as reader:
+        lines = csv.reader(reader)
         for (row, line) in enumerate(lines):
             if row in rows:
                 # Store rows in a matrix.
-                data[i, :] = np.float64(line)
+                data[i, :] = np.float64(line[0:ncol])
                 i += 1
             elif row > max_row:
                 # Stop reading through the file once we've got all rows.
@@ -102,6 +89,54 @@ def countLines(file):
         for line in lines:
             n += 1
     return n
+
+def parseArgs():
+    # Set up command line arguments.
+    parser = ap.ArgumentParser(
+        description = 'This program takes a little bootstrap sample from the \
+        specified CSV data set and fits a linear regression. The last column \
+        of the data set is assumed to be the response. When no arguments are \
+        specified, the mini data set is used, provided it is in the data \
+        sub-directory.')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-i', metavar = 'JOB',
+                       help = 'job number (using the full data set)')
+    group.add_argument('-f', nargs = 4, metavar = ('N', 'P', 'IN', 'OUT'),
+                       help = 'use data set IN with N observations and P \
+                       covariates; results are written to OUT')
+    args = parser.parse_args()
+
+    if args.i:
+        # Use the full data set.
+        index = int(args.i)
+        s_index = (index // R) + 1
+        r_index = index % R
+        # The little bootstrap subset must be the same for all jobs with the
+        # same s_index, while the little bootstrap sample created from the
+        # subset must be different for all jobs.
+        # So the first seed depends on s_index, the second depends on index.
+        seeds = (4141 * s_index, 1157 * index)
+        out_file = 'output/coef_{0:02d}_{1:02d}.txt'.format(s_index, r_index)
+
+        print 'Using full data set. This is job {0} (s = {1}, r = {2}).' \
+            .format(args.i, s_index, r_index)
+        print 'Random seeds are {0} and {1}.\n'.format(seeds[0], seeds[1])
+        return (FULL_N, FULL_P, FULL_IN, out_file, seeds)
+    elif args.f:
+        # Use the user-specified data set.
+        (n, p, in_file, out_file) = args.f
+        n = int(n)
+        p = int(p)
+
+        print 'Using input file:'
+        print 2 * ' ' + in_file
+        print 'Using output file:'
+        print 2 * ' ' + out_file + '\n'
+        return (n, p, in_file, out_file, None)
+    else:
+        # Use the mini data set.
+        print 'Using mini data set (no arguments specified).\n'
+        return (MINI_N, MINI_P, MINI_IN, MINI_OUT, None)
 
 # Allow this script to be loaded as a module.
 if __name__ == '__main__':
