66495f2ebdce1c2d608bdbd8708d6dbc5be098d8
Nick Ulle <nick.ulle@gmail.com>
2013-11-27 11:41:00 -0800

    Final update for HW3.

--- HW3/root_finders.py
+++ HW3/root_finders.py
@@ -1,11 +1,15 @@
-#!/bin/usr/env python
+#!/usr/bin/env python2
 # root_finders.py
 # Author: Nick Ulle
 # Description:
-#   This module includes implementations of the bisection algorithm and the
-#   Newton-Raphson algorithm for root-finding.
+'''Root-finding methods.
+
+This module includes implementations of the bisection algorithm and the
+Newton-Raphson algorithm for root-finding.
+'''
 
 from __future__ import division
+import math
 
 def root_bisection(f, interval, tol = 10 ** -6, max_iter = 10 ** 6, 
                    verbose = False):
@@ -76,14 +80,52 @@ def root_newton(f, df, x0, tol = 10 ** -6, max_iter = 10 ** 6,
     for iter in range(1, max_iter + 1):
         # This loop could be made marginally faster by reducing the number of
         # calls to f, but at the cost of clarity.
-        x = x - (f(x) / df(x))
-
         if verbose and iter % verbose == 0:
             print 'Iteration {0}:'.format(iter)
             print '  f({0:.4g}) = {1:.4g}'.format(x, f(x))
+            print '  df({0:.4g}) = {1:.4g}'.format(x, df(x))
 
         if abs(f(x)) < tol:
             break
 
+        x = x - (f(x) / df(x))
+
     return (x, f(x), iter)
+
+def main():
+    '''Demo the algorithms on Rao\'s linkage problem.'''
+    print '\nRunning as a script.'
+    print 'MLEs for Rao\'s linkage problem will be computed.'
+    print 'Use `import root_finders` to use the root-finding functions' + \
+        ' directly.\n'
+
+    def log_likelihood(x):
+        return 125*math.log(2 + x) + 38*math.log(1 - x) + 34*math.log(x)
+
+    def score(x):
+        return 125 / (2 + x) - 38 / (1 - x) + 34 / x
+
+    def d_score(x):
+        return -125 / (2 + x)**2 - 38 / (1 - x)**2 - 34 / x**2
+
+    TOL = 10**-10
+
+    print 'On (-2, 0):'
+    (root, _, iter) = root_bisection(score, (-1.9999, -0.0001), tol = TOL)
+    print '  Bisection found {0:.4} in {1} iterations' \
+        .format(root, iter)
+    (root, _, iter) = root_newton(score, d_score, -1, tol = TOL)
+    print '  Newton-Raphson found {0:.4} in {1} iterations' \
+        .format(root, iter)
+
+    print 'On (0, 1):'
+    (root, _, iter) = root_bisection(score, (0.0001, 0.9999), tol = TOL)
+    print '  Bisection found {0:.4} in {1} iterations, value is {2:.4}' \
+        .format(root, iter, log_likelihood(root))
+    (root, _, iter) = root_newton(score, d_score, 0.5, tol = TOL)
+    print '  Newton-Raphson found {0:.4} in {1} iterations, value is {2:.4}' \
+        .format(root, iter, log_likelihood(root))
+
+if __name__ == '__main__':
+    main()
 
