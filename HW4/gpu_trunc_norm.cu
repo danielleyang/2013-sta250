f9e8b2013b06605634a93a15b1f71f702547d4c8
Nick Ulle <nick.ulle@gmail.com>
2013-12-08 10:38:50 +0000

    Refine HW4 truncated normal code.

--- HW4/gpu_trunc_norm.cu
+++ HW4/gpu_trunc_norm.cu
@@ -5,9 +5,10 @@
 #include <math.h>
 #include <curand_kernel.h>
 
+#define NUM_RNG 128
 #define MAX_ITER 100
 
-__device__ float tail_normal(
+__device__ float tail_norm(
     float mean, 
     float sd, 
     float a, 
@@ -40,41 +41,66 @@ __device__ float tail_normal(
     return sd * tail * z + mean;
 }
 
-extern "C" {
-__global__ void gpu_trunc_normal(
-    int n, 
-    float *mean, 
-    float *sd,
-    float *a,
-    float *b,
-    float *result)
+__device__ int get_thread_id()
 {
     // Get block and thread numbers.
     int block = blockIdx.x + blockIdx.y * gridDim.x;
     int thread = threadIdx.x + threadIdx.y * blockDim.x
         + threadIdx.z * (blockDim.y * blockDim.x);
     int block_size = blockDim.x * blockDim.y * blockDim.z;
-    int idx = thread + block * block_size;
+    return thread + block * block_size;
+}
+
+extern "C" {
+__global__ void gpu_random_activate(int seed, curandState_t **state)
+{
+    int idx = get_thread_id();
+
+    if (idx < NUM_RNG)
+    {
+        curandState_t *rng_state = \
+            (curandState_t*) malloc(sizeof(curandState_t));
+        curand_init(seed, idx, 0, rng_state);
+        state[idx] = rng_state;
+    }
+}
+
+__global__ void gpu_random_deactivate(curandState_t **state)
+{
+    int idx = get_thread_id();
+
+    if (idx < NUM_RNG) free(state[idx]);
+}
+
+__global__ void gpu_trunc_norm(
+    int n, 
+    float *mean, 
+    float *sd,
+    float *a,
+    float *b,
+    float *result,
+    curandState_t **state)
+{
+    int idx = get_thread_id();
 
     // Only compute if the index is less than the result length.
     if (idx < n)
     {
-        // Initialize the RNG. This could be done in a separate kernel.
-        curandState_t rng_state;
-        curand_init(109 + 126*block, thread, 0, &rng_state);
+        // Determine which RNG to use.
+        curandState_t *rng_state = state[idx % NUM_RNG];
 
         // Get truncated normal value. Use C. Robert's algorithm if possible.
         float draw = 0;
         if (!isfinite(a[idx]) && b[idx] <= mean[idx]) 
         {
-            draw = tail_normal(mean[idx], sd[idx], b[idx], &rng_state);
+            draw = tail_norm(mean[idx], sd[idx], b[idx], rng_state);
         } else if (!isfinite(b[idx]) && a[idx] >= mean[idx]) 
         {
             /*
             printf("RT %i has a = %f, b = %f, mean = %f\n", idx, a[idx], \
                    b[idx], mean[idx]);
             */
-            draw = tail_normal(mean[idx], sd[idx], a[idx], &rng_state);
+            draw = tail_norm(mean[idx], sd[idx], a[idx], rng_state);
         } else 
         {
             /*
@@ -83,7 +109,7 @@ __global__ void gpu_trunc_normal(
             */
             for (int i = 0; i < MAX_ITER; i++)
             {
-                draw = sd[idx] * curand_normal(&rng_state) + mean[idx];
+                draw = sd[idx] * curand_normal(rng_state) + mean[idx];
 
                 if (a[idx] <= draw && draw <= b[idx]) break;
             }
--- HW4/gpu_trunc_norm.py
+++ HW4/gpu_trunc_norm.py
@@ -2,31 +2,82 @@
 # gpu_trunc_norm.py
 # Author: Nick Ulle
 
+from __future__ import division
+from math import ceil, sqrt
+
 import pycuda.autoinit
 import pycuda.driver as cuda
-from pycuda.compiler import SourceModule
 
 import numpy as np
 
-def main():
-    print "Hello!"
-    mod = cuda.module_from_file('gpu_trunc_norm.ptx')
-    gpu_trunc_normal = mod.get_function('gpu_trunc_normal')
+# ----- PTX Setup
+_gpu_module = cuda.module_from_file('gpu_trunc_norm.ptx')
 
-    # Try getting generating some values.
-    n = np.int32(100)
+_gpu_random_activate = _gpu_module.get_function('gpu_random_activate')
+_gpu_random_deactivate = _gpu_module.get_function('gpu_random_deactivate')
+_gpu_trunc_norm = _gpu_module.get_function('gpu_trunc_norm')
+
+# ----- Globals & Constants
+_rng_state = None
+
+# This should match NUM_RNG in gpu_trunc_norm.cu
+_NUM_THREADS = 128
+
+# ----- Main Script
+
+def gpu_random_activate(seed):
+    global _rng_state, _NUM_THREADS
+
+    if _rng_state:
+        print "RNG already initialized! Use gpu_random_free() to free."
+        return
+
+    seed = np.int32(seed)
+    _rng_state = np.zeros(_NUM_THREADS, np.intp)
+
+    _gpu_random_activate(seed, cuda.Out(_rng_state), 
+                         block = (_NUM_THREADS, 1, 1), grid = (1, 1))
+
+def gpu_random_deactivate():
+    global _rng_state, _NUM_THREADS
+
+    _gpu_random_deactivate(cuda.In(_rng_state), 
+                           block = (_NUM_THREADS, 1, 1), grid = (1, 1))
+    _rng_state = None
+
+def gpu_trunc_norm(n, mean, sd, a, b):
+    blocks = int(ceil(sqrt(n / _NUM_THREADS)))
+    if blocks > 512:
+        raise Exception('Maximum sample size in a single call is 33,554,432.')
+
+    n = np.int32(n)
+    mean = mean.astype(np.float32)
+    sd = sd.astype(np.float32)
+    a = a.astype(np.float32)
+    b = b.astype(np.float32)
+    result = np.zeros(n, np.float32)
+
+    _gpu_trunc_norm(n, cuda.In(mean), cuda.In(sd), cuda.In(a), cuda.In(b),
+                    cuda.Out(result), cuda.In(_rng_state),
+                    block = (_NUM_THREADS, 1, 1), grid = (blocks, blocks))
+    return result
+
+def main():
+    gpu_random_activate(4140)
+
+    # Try generating from a truncated normal. For vectors larger than about
+    # 10^6, numpy.mean becomes numerically unstable.
+    n = np.int32(10 ** 5)
     mean = np.zeros(n, np.float32)
     sd = np.ones(n, np.float32)
-    a = np.ones(n, np.float32) * 10
-    b = np.ones(n, np.float32) / 0
-    res = np.zeros(n, np.float32)
-    print b
+    a = np.ones(n, np.float32) * -np.float32('inf')
+    b = np.ones(n, np.float32) * -8
 
-    gpu_trunc_normal(n, cuda.In(mean), cuda.In(sd), cuda.In(a),
-                     cuda.In(b), cuda.Out(res),
-                     block = (7,1,1), grid = (16,1))
+    res = gpu_trunc_norm(n, mean, sd, a, b)
 
-    print res
+    print np.mean(res)
+
+    gpu_random_deactivate()
 
 if __name__ == '__main__':
     main()
